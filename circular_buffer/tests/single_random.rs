d7028c99ee2f230f3c9e4356723fa096df5f64fe
tokeiya3@Home <net_seed@hotmai.com>
2026-05-31 08:26:43 +0900

    sync

---
+++ circular_buffer/tests/single_random.rs
@@ -0,0 +1,21 @@
+mod drop_observe;
+
+use circular_buffer::fixed::*;
+use circular_buffer::{Error, Result};
+use drop_observe::*;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+
+const SIZE: usize = 24;
+type Fixture = Buffer<Probe, SIZE>;
+
+#[test]
+fn enqueue_dequeue() {
+	let seed = rand::rng().next_u64();
+	dbg!(seed);
+	let mut rng = ChaCha8Rng::seed_from_u64(seed);
+
+	let mut buffer = Fixture::default();
+	let mut facory = MonitorGenerator::default();
+	let mut hash = std::collections::HashMap::<usize, Monitor>::new();
+}
