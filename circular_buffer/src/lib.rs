d7305364927d5a92bbe925cc3dbf6cd157dc448d
Tokeiya <net_seed@hotmail.com>
2026-05-31 12:53:07 +0900

    Expose test-only `test_shared` module for reuse in `circular_buffer` tests.

--- circular_buffer/src/lib.rs
+++ circular_buffer/src/lib.rs
@@ -2,6 +2,13 @@ mod circular_buffer;
 mod error;
 pub mod fixed;
 
+#[cfg(test)]
+#[path = "../tests/drop_observe/mod.rs"]
+mod test_shared;
+
 pub use circular_buffer::CircularBuffer;
 pub use error::Error;
 pub use error::Result;
+
+#[cfg(test)]
+pub(crate) use test_shared::*;
