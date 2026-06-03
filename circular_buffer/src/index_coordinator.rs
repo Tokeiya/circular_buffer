d4775fafef70ee62f0dede62a389156b081e718c
Tokeiya <net_seed@hotmail.com>
2026-06-03 18:35:28 +0900

    sync

--- circular_buffer/src/index_coordinator.rs
+++ circular_buffer/src/index_coordinator.rs
@@ -19,3 +19,82 @@ pub trait IndexCoordinator: Clone + Default {
 		self.len() == self.capacity()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[derive(Default)]
+	struct Dummy {
+		pub len: usize,
+		pub capacity: usize,
+	}
+
+	impl Clone for Dummy {
+		fn clone(&self) -> Self {
+			unimplemented!()
+		}
+	}
+
+	impl IndexCoordinator for Dummy {
+		fn head_index(&self) -> Result<usize> {
+			unimplemented!()
+		}
+
+		fn tail_index(&self) -> Result<usize> {
+			unimplemented!()
+		}
+
+		fn enqueue_index(&mut self) {
+			unimplemented!()
+		}
+
+		fn dequeue_index(&mut self) -> Result<()> {
+			unimplemented!()
+		}
+
+		fn pop_index(&mut self) -> Result<()> {
+			unimplemented!()
+		}
+
+		fn real_to_virtual(&self, idx: usize) -> Result<usize> {
+			unimplemented!()
+		}
+
+		fn virtual_to_real(&self, idx: usize) -> Result<usize> {
+			unimplemented!()
+		}
+
+		fn capacity(&self) -> usize {
+			self.capacity
+		}
+
+		fn len(&self) -> usize {
+			self.len
+		}
+	}
+
+	#[test]
+	fn is_empty() {
+		let mut dummy = Dummy::default();
+		dummy.capacity = 120;
+		assert_eq!(dummy.len(), 0);
+		assert_eq!(dummy.capacity(), 120);
+		assert!(dummy.is_empty());
+
+		for i in 1..100 {
+			dummy.len = i;
+			assert_eq!(dummy.len(), i);
+			assert!(!dummy.is_empty());
+		}
+	}
+
+	#[test]
+	fn is_full() {
+		let mut dummy = Dummy::default();
+		assert_eq!(dummy.len, 0);
+		assert_eq!(dummy.capacity(), 0);
+		assert!(dummy.is_full());
+		assert!(dummy.is_empty());
+	}
+}
