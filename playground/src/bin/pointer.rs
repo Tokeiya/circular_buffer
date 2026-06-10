abf06e00c014f7549a7dcbb7055d311b37307ae4
Tokeiya <net_seed@hotmail.com>
2026-06-10 12:29:58 +0900

    Add `IterMut` struct for mutable iteration over `Envelope` items and implement `Iterator` trait

---
+++ playground/src/bin/pointer.rs
@@ -0,0 +1,31 @@
+use std::marker::PhantomData;
+
+pub struct Envelope<T>(Vec<T>);
+
+struct IterMut<'a, T> {
+	pointer: *mut T,
+	len: usize,
+	capacity: usize,
+	_phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T> IterMut<'a, T> {
+	pub fn new(_: &'a mut Envelope<T>, pointer: *mut T, capacity: usize) -> Self {
+		Self {
+			pointer,
+			len: 0,
+			capacity,
+			_phantom: PhantomData,
+		}
+	}
+}
+
+impl<'a, T: 'a> Iterator for IterMut<'a, T> {
+	type Item = &'a T;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		todo!()
+	}
+}
+
+fn main() {}
